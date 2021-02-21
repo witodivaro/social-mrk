7b03efc620c4b67550efcc4a6ef0f2c11186c3c3
disorientationinspace <disorientationinspace@gmail.com>
2021-02-21 22:23:06 +0300

    feat: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
+import aminGoma from './apis/amin-goma';
+import axios from 'axios';
 
-function App() {
+import RegisterPage from './pages/register-page/register-page.component';
+
+import './App.scss';
+
+const App = () => {
+  useEffect(() => {}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={RegisterPage} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/apis/amin-goma.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://893913c649da.ngrok.io/',
+});
