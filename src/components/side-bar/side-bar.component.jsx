1ebe563c4a736a651159f69a333f6544ad87ad03
disorientationinspace <disorientationinspace@gmail.com>
2021-03-07 20:34:38 +0300

    feat: add side-bar sign out state

--- src/components/side-bar/side-bar.component.jsx
+++ src/components/side-bar/side-bar.component.jsx
@@ -1,38 +1,64 @@
 import './side-bar.styles.scss';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
-import { FaUser, FaNewspaper, FaUsers, FaEnvelope } from 'react-icons/fa';
+import {
+  FaUser,
+  FaNewspaper,
+  FaUsers,
+  FaEnvelope,
+  FaUserPlus,
+  FaSignInAlt,
+} from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 const SideBar = () => {
-  return (
-    <div className="side-bar">
+  const currentUser = useSelector(selectCurrentUser);
+
+  const renderedSideBarContent = useMemo(() =>
+    currentUser ? (
+      <>
+        <p className="side-bar__link-container">
+          <Link className="side-bar__link" to="/">
+            <FaUser className={'side-bar__icon'} />
+            <span className="side-bar__text">Мой профиль</span>
+          </Link>
+        </p>
+        <p className="side-bar__link-container">
+          <Link className="side-bar__link">
+            <FaNewspaper className={'side-bar__icon'} to="/feed" />
+            <span className="side-bar__text">Новости</span>
+          </Link>
+        </p>
+        <p className="side-bar__link-container">
+          <Link className="side-bar__link">
+            <FaEnvelope className={'side-bar__icon'} to="/messages" />
+            <span className="side-bar__text">Сообщения</span>
+          </Link>
+        </p>
+        <p className="side-bar__link-container">
+          <Link className="side-bar__link">
+            <FaUsers className={'side-bar__icon'} to="/friends" />
+            <span className="side-bar__text">Друзья</span>
+          </Link>
+        </p>
+      </>
+    ) : (
       <p className="side-bar__link-container">
-        <Link className="side-bar__link">
-          <FaUser className={'side-bar__icon'} />
-          <span className="side-bar__text">Мой профиль</span>
+        <Link className="side-bar__link" to="/">
+          <FaSignInAlt className={'side-bar__icon'} />
+          <span className="side-bar__text">Вход</span>
+        </Link>
+        <Link className="side-bar__link" to="/">
+          <FaUserPlus className={'side-bar__icon'} />
+          <span className="side-bar__text">Регистрация</span>
         </Link>
       </p>
-      <p className="side-bar__link-container">
-        <Link className="side-bar__link">
-          <FaNewspaper className={'side-bar__icon'} />
-          <span className="side-bar__text">Новости</span>
-        </Link>
-      </p>
-      <p className="side-bar__link-container">
-        <Link className="side-bar__link">
-          <FaEnvelope className={'side-bar__icon'} />
-          <span className="side-bar__text">Сообщения</span>
-        </Link>
-      </p>
-      <p className="side-bar__link-container">
-        <Link className="side-bar__link">
-          <FaUsers className={'side-bar__icon'} />
-          <span className="side-bar__text">Друзья</span>
-        </Link>
-      </p>
-    </div>
+    )
   );
+
+  return <div className="side-bar">{renderedSideBarContent}</div>;
 };
 
 export default SideBar;
